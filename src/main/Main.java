7578199643400955dc662ef62ab9e23ef727e61c
Emilio Martin <emiunrn@gmail.com>
2022-07-16 01:44:25 -0300

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import model.Persona;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Persona persona = new Persona();
+        persona.setEdad(20);
+        persona.setNombre("Emilio");
+        persona.setTelefono("123456");
+    }
+}
